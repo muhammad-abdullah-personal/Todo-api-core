576211626a3022866f8eb86d076f22d0227f5a36
Muhammad Abdullah <muhammad.a@inductionhealthcare.com>
2022-07-21 14:38:28 +0500

    project changed

--- TodoAPI/TodoAPI/WeatherForecast.cs
+++ TodoAPI/TodoAPI/WeatherForecast.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace TodoAPI
+namespace TodoApi
 {
     public class WeatherForecast
     {
@@ -10,6 +8,6 @@ namespace TodoAPI
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
